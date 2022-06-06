7af6a40b919607c16ce4cf8fa4a36f65ad61a16c
Federico Baldini <federico.baldini7@gmail.com>
2022-06-06 21:33:39 +0200

    Update prime.c
    Added a prime numbers checker function.

--- libraries/prime.c
+++ libraries/prime.c
@@ -11,3 +11,16 @@ int is_prime(int number)
   return (number != 1 && number != 0) ? 1 : 0;
 };
 
+// Check numbers 0 - {primes}
+int check_primes(int number)
+{
+  int count = 0;
+  for (int i = 0; i < number; i++)
+  {
+    if (is_prime(i))
+    {
+      count += 1;
+    }
+  }
+  return count;
+};
